e93c08ae4a0ffd7fcc95ab6eab057a1e876c6859
Mandy He <hemandy329@gmail.com>
2021-04-14 17:00:26 -0400

    edited editProfile, able to delete account and change username, password, and profile image

--- frontend/src/components/ProfilePic.js
+++ frontend/src/components/ProfilePic.js
@@ -2,23 +2,55 @@ import React, { useState, useEffect } from "react";
 import firebase from "firebase";
 
 const ProfilePic = (props) => {
-    const [imageUrl, setImageUrl] = useState(null);
-    //const logo = require('../images/default_profile.png');
     const user = props.user;
-    // const logo = require(user.photoURL);
-    // const storage = firebase.storage().ref()
+    const [imageUrl, setImageUrl] = useState(user.photoURL);
+
 
     let storage = firebase.storage();
     // Create a reference to the file we want to download
     let gsReference = storage.refFromURL('gs://short-demo-login.appspot.com/default_profile.png');
 
-    const getDefaultImage = () => {
+    // const getDefaultImage = () => {
+    //     gsReference.getDownloadURL()
+    //         .then((url) => {
+    //             // Insert url into an <img> tag to "download"
+    //             console.log(user.photoURL ? user.photoURL : url)
+    //             setImageUrl(url);
+    //             //return <img src={url} alt=""/>;
+    //         })
+    //         .catch((error) => {
+    //             // A full list of error codes is available at
+    //             // https://firebase.google.com/docs/storage/web/handle-errors
+    //             switch (error.code) {
+    //                 case 'storage/object-not-found':
+    //                     // File doesn't exist
+    //                     console.log(error.code + "File doesn't exist")
+    //                     break;
+    //                 case 'storage/unauthorized':
+    //                     // User doesn't have permission to access the object
+    //                     console.log(error.code + ": User doesn't have permission to access the object")
+    //                     break;
+    //                 case 'storage/canceled':
+    //                     // User canceled the upload
+    //                     console.log(error.code + ": User canceled the upload")
+    //                     break;
+    //                 case 'storage/unknown':
+    //                     // Unknown error occurred, inspect the server response\
+    //                     console.log(error.code + ": Unknown error occurred, inspect the server response")
+    //                     break;
+    //             }
+    //         });
+    // }
+
+    const setDefaultImage = () => {
         gsReference.getDownloadURL()
             .then((url) => {
-                // Insert url into an <img> tag to "download"
-                console.log(user.photoURL ? user.photoURL : url)
                 setImageUrl(url);
-                //return <img src={url} alt=""/>;
+                user.updateProfile({
+                    photoURL: url       // <- URL from uploaded photo.
+                }).then(r => {
+                    console.log(r);
+                });
             })
             .catch((error) => {
                 // A full list of error codes is available at
@@ -45,20 +77,20 @@ const ProfilePic = (props) => {
     }
 
     useEffect(() => {
-            if (user.photoURL) {
-                setImageUrl(user.photoURL);
+            if (imageUrl) {
+                setImageUrl(imageUrl);
             } else {
-                setImageUrl(getDefaultImage());
+                setImageUrl(setDefaultImage());
             }
         }
-        , []);
+        , [imageUrl]);
 
-    useEffect(() => {
-            if (user.photoURL) {
-                setImageUrl(user.photoURL);
-            }
-        }
-        , [user.photoURL]);
+    // useEffect(() => {
+    //         if (user.photoURL) {
+    //             setImageUrl(user.photoURL);
+    //         }
+    //     }
+    //     , [user.photoURL]);
 
     return (
         <img src={imageUrl} alt="Profile-pic could not load" className={props.className}/>
--- frontend/src/pages/EditProfile.js
+++ frontend/src/pages/EditProfile.js
@@ -35,16 +35,25 @@ function EditProfile(props) {
         setLogMessage('');
     }
 
+    // const removeUserAndData = () => {
+    //     // //remove user data
+    //     // db.collection("users").document(user.uid).delete();
+    //     // remove account
+    //     user.delete().then(function() {
+    //         console.log("user successfully deleted")
+    //     }).catch(function(error) {
+    //         console.log("user was not successfully deleted"+      error)
+    //     });
+    //     setDeleteAccount(true);
+    // }
+
     const removeUserAndData = () => {
-        // //remove user data
-         db.collection("users").document(user.uid).delete();
-        // remove account
-        user.delete().then(function() {
-            console.log("user successfully deleted")
-        }).catch(function(error) {
-            console.log("user was not successfully deleted"+      error)
+        user.delete().catch(function(error) {
+            if (error.code === 'auth/requires-recent-login') {
+                window.alert('Please sign-in and try again.');
+                firebase.auth().signOut();
+            }
         });
-        setDeleteAccount(true);
     }
 
     const updateProfile = () => {
@@ -52,33 +61,24 @@ function EditProfile(props) {
         updatePassword();
         user.updateProfile({
             displayName: newName !== "" ? newName: user.displayName,
-            photoURL: newPicURL,
         }).then(function() {
-            console.log("user successfully updated to " + newName)
-            setLogMessage(logMessage  + "user info successfully updated")
+            console.log("user successfully updated:  " + logMessage)
+            console.log(user)
             // newPassword ? user.updatePassword(newPassword) : null;
             // newEmail ? user.updateEmail(newEmail) : null;
         }).catch(function(error) {
-            console.log("user not successfully updated"+  error)
+            console.log( error)
+            console.log(user)
+            console.log(firebase.storage().ref('users/' + user.uid +'/profile.jpg').getDownloadURL())
         });
         clearInputs();
     }
+
     // useEffect(() => {
-    //     if (newEmail !== "")
-    //         user.updateEmail(newEmail)
-    //             .then(r =>  {
-    //                 console.log(r + ":user successfully updated Email")
-    //                 setLogMessage(logMessage + "<br>" + "user successfully updated Email " + newEmail)
-    //             })
-    //             .catch(err => {
-    //                 switch(err.code) {
-    //                     case "auth/email-already-in-use":
-    //                     case "auth/invalid-email":
-    //                         setLogMessage(err.message);
-    //                         break;
-    //                 }
-    //             });
-    //     }, [newEmail]);
+    //     firebase.storage().ref('users/' + user.uid +'/profile.jpg').put(newPicURL);
+    //     setLogMessage("user image updated")
+    //     updateProfile();
+    //     }, [newPicURL]);
 
     function updatePassword() {
         if (newPassword !== "")
@@ -110,10 +110,26 @@ function EditProfile(props) {
                         });
     }
 
-
+    const profilePicRef = firebase.storage().ref('users/' + user.uid +'/profile.jpg')
+//TODO:
+    //sources: https://stackoverflow.com/questions/45386065/firebase-user-photourl-to-string
+    // https://www.youtube.com/watch?v=31MVIwvstzs&ab_channel=SoftAuthor
     function getNewPicUrl(e) {
-        setNewPicURL(e.target.files[0])
-        updateProfile();
+        setNewPicURL(e.target.files[0]);
+        profilePicRef.put(e.target.files[0])
+            .then(function(snapshot) {
+                console.log(snapshot)
+                snapshot.ref.getDownloadURL()
+                    .then(function(url) {  // Now I can use url
+                        user.updateProfile({
+                            photoURL: url       // <- URL from uploaded photo.
+                        }).then(r => {
+                            setLogMessage("user image updated" + r)
+                        });
+                    })
+            })
+        console.log("user image updated")
+        //updateProfile();
     }
 
     return (
